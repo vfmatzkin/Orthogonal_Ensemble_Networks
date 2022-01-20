4791b3241e102e4e8a14933ff5a9d2eccf61ff0a
matzkin <vfmatzkin@gmail.com>
2022-01-20 15:09:08 -0300

    First Franco commit - Removed unicode characters that the cluster encoding did not understand. - Added n_classes option in the config_file for setting this parameter in the ResUNet_model. - Added symbolic link to the config file so it points to a determined dataset config file (config_file.ini is hardcoded in some places).

--- ResUNet_model.py
+++ ResUNet_model.py
@@ -1,12 +1,9 @@
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.layers import  Conv3D, Activation, MaxPooling3D, Conv3DTranspose, Add,BatchNormalization, Dropout, Input, concatenate
+from tensorflow.keras.layers import Conv3D, MaxPooling3D, \
+    Conv3DTranspose, Input, concatenate
 
 
-def build_network():
-
-    inputs = Input((None, None, None, 2))
+def build_network(n_classes):
+    inputs = Input((None, None, None, n_classes))
     conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(inputs)
     conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conv1)
     conc1 = concatenate([inputs, conv1], axis=4)
@@ -31,32 +28,30 @@ def build_network():
     conv5 = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(conv5)
     conc5 = concatenate([pool4, conv5], axis=4)
 
-    up6 = concatenate([Conv3DTranspose(256, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc5), conv4], axis=4)
+    up6 = concatenate([Conv3DTranspose(256, (2, 2, 2), strides=(2, 2, 2),
+                                       padding='same')(conc5), conv4], axis=4)
     conv6 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(up6)
     conv6 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(conv6)
     conc6 = concatenate([up6, conv6], axis=4)
 
-    up7 = concatenate([Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc6), conv3], axis=4)
+    up7 = concatenate([Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2),
+                                       padding='same')(conc6), conv3], axis=4)
     conv7 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(up7)
     conv7 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conv7)
     conc7 = concatenate([up7, conv7], axis=4)
 
-    up8 = concatenate([Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc7), conv2], axis=4)
+    up8 = concatenate([Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2),
+                                       padding='same')(conc7), conv2], axis=4)
     conv8 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(up8)
     conv8 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(conv8)
     conc8 = concatenate([up8, conv8], axis=4)
 
-    up9 = concatenate([Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc8), conv1], axis=4)
+    up9 = concatenate([Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2),
+                                       padding='same')(conc8), conv1], axis=4)
     conv9 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(up9)
     conv9 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conv9)
     conc9 = concatenate([up9, conv9], axis=4)
 
     conv10 = Conv3D(1, (1, 1, 1), activation='sigmoid')(conc9)
 
-
     return inputs, conv10
-
-
-
-
-
